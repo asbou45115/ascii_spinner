0a49b754347cab4918b62793603bbb59c93a5ba2
asbou45115 <117445278+asbou45115@users.noreply.github.com>
2025-10-02 19:56:52 +1000

    removed Cube.py

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from src.AsciiObj import Cube, TriangularPyramid
 def main():
     renderer = Renderer(width=80, height=40, dist_from_screen=60)
     # cube = Cube(renderer)
-    
     # cube.run_on_terminal()       
+
     tri_py = TriangularPyramid(renderer)
     tri_py.run_on_terminal()
 
