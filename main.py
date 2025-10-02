878f2e78d6513c93967982a86ae576062c148a1b
asbou45115 <117445278+asbou45115@users.noreply.github.com>
2025-10-02 18:50:12 +1000

    Feat: created spinning cube

--- main.py
+++ main.py
@@ -5,25 +5,56 @@ from src.Rotation import Rotation
 def main():
     renderer = Renderer(width=80, height=40, dist_from_screen=60)
     
-    step = 1
-    surface_points = [(x, y, 0) for x in [i * step for i in range(-10, 11)]
-                                 for y in [j * step for j in range(-10, 11)]]
+    step = 0.8
+    size = 12
+    
+    surfaces = []
+    
+    # Front face - #
+    for i in [j * step for j in range(-size, size + 1)]:
+        for k in [j * step for j in range(-size, size + 1)]:
+            surfaces.append((i, k, size * step, '#'))
+    
+    # Back face - @
+    for i in [j * step for j in range(-size, size + 1)]:
+        for k in [j * step for j in range(-size, size + 1)]:
+            surfaces.append((i, k, -size * step, '@'))
+    
+    # Left face - *
+    for i in [j * step for j in range(-size, size + 1)]:
+        for k in [j * step for j in range(-size, size + 1)]:
+            surfaces.append((-size * step, i, k, '*'))
+    
+    # Right face - +
+    for i in [j * step for j in range(-size, size + 1)]:
+        for k in [j * step for j in range(-size, size + 1)]:
+            surfaces.append((size * step, i, k, '+'))
+    
+    # Top face - =
+    for i in [j * step for j in range(-size, size + 1)]:
+        for k in [j * step for j in range(-size, size + 1)]:
+            surfaces.append((i, size * step, k, '$'))
+    
+    # Bottom face - -
+    for i in [j * step for j in range(-size, size + 1)]:
+        for k in [j * step for j in range(-size, size + 1)]:
+            surfaces.append((i, -size * step, k, '-'))
     
     angle = 0
     while True:
-        rotation = Rotation(angle, angle * 0.7, angle * 0.3)
+        rotation = Rotation(angle, angle*0.7, angle*0.3)
         renderer.clear_frame()
         
-        for x, y, z in surface_points:
+        for x, y, z, char in surfaces:
             rx, ry, rz = rotation.rotate_point(x, y, z)
             px, py, pz = renderer.project(rx, ry, rz)
-            renderer.set_pixel(px, py, pz, "#")
+            renderer.set_pixel(px, py, pz, char)
             
         renderer.clear()
         renderer.render()
 
         angle += 0.05
-        time.sleep(0.05)
+        time.sleep(0.02)
 
 if __name__ == "__main__":
     main()
